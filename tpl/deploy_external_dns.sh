4df7ad0c32d4e4d28ee4cb6418c6d91c7e201ba3
Seun Oluwaloju <seunoluwaloju@Seuns-MacBook-Pro.local>
2020-11-09 15:24:09 -0600

    [Seun | Nelson] - fix external dns

--- tpl/deploy_external_dns.sh
+++ tpl/deploy_external_dns.sh
@@ -2,7 +2,7 @@
 
 export AWS_ACCOUNT_ID=$(secrethub read vapoc/platform/svc/aws/aws-account-id)
 
-aws sts assume-role --output json --role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/DPSTerraformRole --role-session-name deploy-external-dns-session > credentials
+aws sts assume-role --output json --role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/DPSTerraformRole --role-session-name deploy-external-dns-session >credentials
 export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
@@ -10,17 +10,17 @@ export AWS_DEFAULT_REGION=us-west-2
 
 export cluster=$1
 if [[ $cluster == 'preview' ]]; then
-  host='*.devportal.com'
+  host='devportal.com'
 fi
 
 if [[ $cluster == 'sandbox' ]]; then
-  host="*.$cluster.devportal.name"
+  host="{$1}.devportal.name"
 fi
 
 export HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name $host | jq -r --arg DNS $host '.HostedZones[] | select( .Name | contains($DNS)) | .Id')
 
 # external-dns deployment files
-cat <<EOF > external-dns-deployment.yaml
+cat <<EOF >external-dns-deployment.yaml
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -90,7 +90,7 @@ spec:
         - --source=ingress
         - --source=istio-gateway
         - --source=istio-virtualservice
-        - --domain-filter=${1}.devportal.name
+        - --domain-filter=$host
         - --provider=aws
         # - --policy=upsert-only # would prevent ExternalDNS from deleting any records, omit to enable full synchronization
         - --aws-zone-type=public # only look at public hosted zones (valid values are public, private or no value for both)
