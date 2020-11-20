81e01d8022ac85419060948532a706e58e0d36cd
Punit Lad <plad@thoughtworks.com>
2020-11-20 14:33:10 -0500

    [Punit | Richard] helm deploy cert manager

--- tpl/deploy_cert_manager.sh
+++ tpl/deploy_cert_manager.sh
@@ -9,4 +9,4 @@ kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-#helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true
+helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true
