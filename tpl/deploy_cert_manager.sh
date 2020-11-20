de8327bf4eb808ed15e15ef7c6f508231fa984a7
Richard <Richardguaman4@gmail.com>
2020-11-20 15:15:39 -0500

    [Richard | Punit] Added annotation for correct role

--- tpl/deploy_cert_manager.sh
+++ tpl/deploy_cert_manager.sh
@@ -4,9 +4,10 @@
 # $1 = cluster config to use
 
 export CERT_MANAGER_VERSION=$(cat tpl/${1}.json | jq -r '.cert_manager_version')
+export AWS_ACCOUNT_ID=$(secrethub read vapoc/platform/svc/aws/aws-account-id)
 
 kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true
+helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=arn:aws:iam::${AWS_ACCOUNT_ID}:role/${1}-cert-manager -f values.cert_manager.yaml
