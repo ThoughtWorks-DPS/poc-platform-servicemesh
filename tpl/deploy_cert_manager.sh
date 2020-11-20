538f54f507745ad59465c664fe47b4ffab3aae5a
Richard <Richardguaman4@gmail.com>
2020-11-20 15:21:45 -0500

    [Richard | Punit] Corrected file reference

--- tpl/deploy_cert_manager.sh
+++ tpl/deploy_cert_manager.sh
@@ -10,4 +10,4 @@ kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=arn:aws:iam::${AWS_ACCOUNT_ID}:role/${1}-cert-manager -f values.cert_manager.yaml
+helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --version v${CERT_MANAGER_VERSION} --set installCRDs=true --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=arn:aws:iam::${AWS_ACCOUNT_ID}:role/${1}-cert-manager -f tpl/values.cert_manager.yaml
